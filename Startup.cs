6adbf0389d45bbc5bc1b24c690472c2a1cc8f101
taujiong <taujiong@outlook.com>
2021-06-28 21:02:09 +0800

    feat: add authentication based on indentityserver for IdentityServer.Admin

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -9,24 +10,29 @@ namespace AtomicSharp.IdentityServer.Admin
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = Configuration["OAuth:Authority"];
+                    options.Audience = Configuration["OAuth:Audience"];
+                });
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "AtomicSharp.IdentityServer.Admin", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = Configuration["App:AppName"], Version = "v1"});
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,13 +40,14 @@ namespace AtomicSharp.IdentityServer.Admin
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AtomicSharp.IdentityServer.Admin v1"));
+                    c.SwaggerEndpoint($"{Configuration["App:AppUrl"]}/swagger/v1/swagger.json", $"{Configuration["App:AppName"]} v1"));
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
